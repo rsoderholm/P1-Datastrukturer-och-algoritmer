1edcf6f782d20768ee129a78ccf3b2db439dea7d
rsoderholm <robin.soderholm87@gmail.com>
2016-09-07 15:53:25 +0200

    Testar lite

--- P1/src/Stack.java
+++ P1/src/Stack.java
@@ -4,14 +4,27 @@
 public class Stack {
     private int countElements;
     private ListNode firstNode = null;
+    private ListNode newNode;
+    private Object dataIn;
 
 
-    public void push() {
+
+    public void push (Object dataIn){
+        if(firstNode == null) {
+            newNode = new ListNode(dataIn, firstNode);
+
+        }
+
 
     }
 
 
-    public void pop() {
+    public Object pop() {
+        if(firstNode == null) {
+            return -1;
+        } else {
+            Object dataIn = 
+        }
 
     }
 
