5130391865f6286cfcee8ae360c48af2aff05964
rsoderholm <robin.soderholm87@gmail.com>
2016-09-07 15:18:41 +0200

    Metoder

---
+++ P1/src/Stack.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Robin on 2016-09-07.
+ */
+public class Stack {
+    private int countElements;
+    private ListNode firstNode = null;
+
+
+    public void push() {
+
+    }
+
+
+    public void pop() {
+
+    }
+
+    public void peek() {
+
+    }
+}
+
